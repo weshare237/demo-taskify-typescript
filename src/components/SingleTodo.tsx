1fe347ad205864f6d8b2305e02cc9560e7199828
weshare237 <fopaduclair2000@gmail.com>
2022-06-30 09:53:29 +0100

    Drag and drop

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
 import './styles.css'
+import { Draggable } from 'react-beautiful-dnd'
 
 interface Props {
+  index: number
   todo: Todo
   todos: Todo[]
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
-const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+const SingleTodo: React.FC<Props> = ({ index, todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<boolean>(false)
   const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
@@ -40,40 +42,57 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setEdit(false)
   }
 
-  return (
-    <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
-      {edit ? (
-        <input
-          type='text'
-          value={editTodo}
-          onChange={(e) => setEditTodo(e.target.value)}
-          className='todos__single--text'
-        />
-      ) : todo.isDone ? (
-        <s className='todos__single--text'>{todo.todo}</s>
-      ) : (
-        <span className='todos__single--text'>{todo.todo}</span>
-      )}
+  const inputRef = useRef<HTMLInputElement>(null)
 
-      <div>
-        <span
-          className='icon'
-          onClick={() => {
-            if (!edit && !todo.isDone) {
-              setEdit(!edit)
-            }
-          }}
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+
+  return (
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <form
+          className='todos__single'
+          onSubmit={(e) => handleEdit(e, todo.id)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <AiFillEdit />
-        </span>
-        <span className='icon' onClick={() => handleDelete(todo.id)}>
-          <AiFillDelete />
-        </span>
-        <span className='icon' onClick={() => handleDone(todo.id)}>
-          <MdDone />
-        </span>
-      </div>
-    </form>
+          {edit ? (
+            <input
+              type='text'
+              ref={inputRef}
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className='todos__single--text'
+            />
+          ) : todo.isDone ? (
+            <s className='todos__single--text'>{todo.todo}</s>
+          ) : (
+            <span className='todos__single--text'>{todo.todo}</span>
+          )}
+
+          <div>
+            <span
+              className='icon'
+              onClick={() => {
+                if (!edit && !todo.isDone) {
+                  setEdit(!edit)
+                }
+              }}
+            >
+              <AiFillEdit />
+            </span>
+            <span className='icon' onClick={() => handleDelete(todo.id)}>
+              <AiFillDelete />
+            </span>
+            <span className='icon' onClick={() => handleDone(todo.id)}>
+              <MdDone />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
   )
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,25 +2,69 @@ import React from 'react'
 import './styles.css'
 import { Todo } from '../model'
 import SingleTodo from './SingleTodo'
+import { Droppable } from 'react-beautiful-dnd'
 
 interface Props {
   todos: Todo[]
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+  completedTodos: Todo[]
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
-const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+const TodoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+}) => {
   return (
-    <div className='todos'>
-      {todos.map((todo) => {
-        return (
-          <SingleTodo
-            todo={todo}
-            key={todo.id}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        )
-      })}
+    <div className='container'>
+      <Droppable droppableId='TodosList'>
+        {(provided) => (
+          <div
+            className='todos'
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className='todos__heading'>Active Tasks</span>
+            {todos.map((todo, index) => {
+              return (
+                <SingleTodo
+                  index={index}
+                  todo={todo}
+                  key={todo.id}
+                  todos={todos}
+                  setTodos={setTodos}
+                />
+              )
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId='TodosRemove'>
+        {(provided) => (
+          <div
+            className='todos remove'
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className='todos__heading'>Completed Tasks</span>
+            {completedTodos.map((todo, index) => {
+              return (
+                <SingleTodo
+                  index={index}
+                  todo={todo}
+                  key={todo.id}
+                  todos={completedTodos}
+                  setTodos={setCompletedTodos}
+                />
+              )
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   )
 }
